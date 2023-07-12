80a3cdef45608ef660e2726d78a6ce18e28aecf1
Hafeefa <hafeefa40@gmail.com>
2023-07-12 16:08:58 -0400

    Add initial game state and game reducer with cutscene and option handling

---
+++ src/redux/user/game1Actions.js
@@ -0,0 +1,21 @@
+//this action creator will set the current cutscene in the game state
+export const setCutScene = (cutScene) => {
+    return{
+        //'SET_OUTCOME' is the action type, which indicates we are setting the cutscene itself
+        type: 'SET_CUTSCENE', //did this way instead of making a new actiontypes action file
+        //payload is the data associated with the action, and in this type of case, it will be the outcome itself
+        payload: cutScene
+    }
+}
+
+//Action creator that will handle the users choice of an option during the cutscenes
+export const option = (optionScreen) => {
+    return {
+        //since SET_OPTIONAL is an action type, it indicates whether the user is choosing an option
+        type:'SET_OPTION',
+        //since payload is the data associated with the action, here, this is the chosen option
+        payload: optionScreen
+    }
+} 
+
+//basically, setCutScene is the cut scene and the optionScreen is where the user can select an option on the screen.
