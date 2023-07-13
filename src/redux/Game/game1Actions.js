130ac69fb007ae4a580f47a19ae15f90cd267ed8
Hafeefa <hafeefa40@gmail.com>
2023-07-12 20:14:33 -0400

    made the initial gameplay and included fakepapi

--- src/redux/Game/game1Actions.js
+++ src/redux/Game/game1Actions.js
@@ -3,7 +3,7 @@ export const setCutScene = (cutScene) => {
     return{
         //'SET_OUTCOME' is the action type, which indicates we are setting the cutscene itself
         type: 'SET_CUTSCENE', //did this way instead of making a new actiontypes action file
-        //payload is the data associated with the action, and in this type of case, it will be the outcome itself
+        //payload is the data associated with the action, and in this type of case, it will be the outcome itself,
         payload: cutScene
     }
 }
@@ -17,5 +17,4 @@ export const option = (optionScreen) => {
         payload: optionScreen
     }
 } 
-
 //basically, setCutScene is the cut scene and the optionScreen is where the user can select an option on the screen.
