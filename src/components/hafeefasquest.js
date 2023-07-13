8d0c5224962e60c9236a111efe6a9274ccf76204
Hafeefa <hafeefa40@gmail.com>
2023-07-12 22:36:27 -0400

    added fake api, added some navbar styling

--- src/components/hafeefasquest.js
+++ src/components/hafeefasquest.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import {cutScene, setCutScene, option, optionScreen} from '../redux/Game/game1Actions'
 
-export const Gameplay = () => {
+ const Gameplay = () => {
     //always declare the dispatch
     const dispatch = useDispatch();
     //get the cutScene and optionScreen using useSelector, which will be grabbed from the redux store which is essentially what useSelector does
@@ -16,8 +16,8 @@ export const Gameplay = () => {
             try{
                 //using a fake api call for now:
                 const response = fakeAPIRequest(); //method is below
-                //dispatch is the mailman. dispatch the cutScene and optionScreen:
-                 dispatch(setCutScene(response.cutScene))
+                //dispatch is the mailman. dispatch the cutScene :
+                 dispatch(setCutScene(response.cutScene));
             } catch (error) {
                 console.log(error)
             }
@@ -25,19 +25,35 @@ export const Gameplay = () => {
         //fake api request that will include a delay thats done with a promise:
         const fakeAPIRequest = () => {
             return new Promise((resolve) => {
-                //going to set a delay of 1000 milliseconds, which is actually 1 second
+                //set a delay of 1000 milliseconds, which is actually 1 second
                 setTimeout(() => {
                     const data = {
                         cutScene: "Todoroki gets super shy and fires his powers at you. You die a burning death.",
                         options: [],
                     };
                     resolve(data);
+                    console.log(data , "you've reached the data")
                 }, 1000);
             })
         }; 
-        
+
             fetchData();
         }, [dispatch]);
+        
 
-        return null;
+        return (
+            <>
+                <h1> Quest </h1>
+                <p> {cutScene} and Options: </p>
+                <ul>
+                    {/* iterate through the array of optionScreen */}
+                    {optionScreen.map((option, index)=>{
+                        console.log(option);
+                        <li key = {index}> {option} </li>;
+                    })};
+                </ul>
+            </>
+        )
     };
+
+export default Gameplay;
