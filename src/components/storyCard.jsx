063229effb2be8e107b5d7d30d6ee407f6f2c716
Hafeefa <hafeefa40@gmail.com>
2023-07-14 12:36:13 -0400

    added the StoriesPage component with story list map and loading state

---
+++ src/components/storyCard.jsx
@@ -0,0 +1,12 @@
+//need props, which is why we imported storycard into storiespage
+import React from 'react'
+
+function storyCard() {
+  return (
+    <div>
+        storyCard
+    </div>
+  )
+}
+
+export default storyCard
