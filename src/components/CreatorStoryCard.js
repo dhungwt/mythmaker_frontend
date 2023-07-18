a6c9770823a4a7d700767e6fa73bcbfd5443dc47
dhungwt <112122672+dhungwt@users.noreply.github.com>
2023-07-18 11:02:41 -0400

    added a working edit button on the user homepage that redirects to edit an existing story

--- src/components/CreatorStoryCard.js
+++ src/components/CreatorStoryCard.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import UserHomepage from '../pages/UserHomepage';
+import React from "react";
+import UserHomepage from "../pages/UserHomepage";
 
 function CreatorStoryCard(props) {
-    const creatorStory = props.creatorIdSelector;
-        console.log(creatorStory, "i am the creator story")
-        
+  const creatorStory = props.creatorIdSelector;
+  console.log(creatorStory, "i am the creator story");
+
+  const handleEditStoryClick = () => {
+    console.log("INSIDE EDIT STORY CLICK HANDLER");
+    window.location.href = `./createStory/${creatorStory._id}`;
+  };
+
   return (
-    <div>CreatorStoryCard:   title:    {creatorStory.title}</div>
-  )
+    <div>
+      CreatorStoryCard: Title: {creatorStory.title}
+      <button onClick={handleEditStoryClick}>✎ Edit ✎</button>
+    </div>
+  );
 }
 
-export default CreatorStoryCard
+export default CreatorStoryCard;
