6c41d3ab02808b864bf7ade433c6f89088e199b2
dhungwt <112122672+dhungwt@users.noreply.github.com>
2023-07-27 14:03:32 -0400

    adding StoryCard back

---
+++ src/components/StoryCard.jsx
@@ -0,0 +1,77 @@
+//need props, which is why we imported storycard into storiespage
+// import React, { useEffect, useState } from 'react'
+// import './components.css'
+
+// function StoryCard(props) {
+//     const story = props.story;
+
+//     const handleClick = (e) =>{
+//         e.preventDefault();
+//         window.location.href= `/stories/:id`; //redirects to the individual story page
+//     }
+//   return (
+//     <div className="story-card" style={{maxWidth:"20%", height:"10%"}}>
+//         <button className="card-button" onClick={handleClick}>
+//         <h1 className="card-title"> Title: {story.title} </h1>
+//         {/* <p> Event: {event} </p> */}
+//         <div key={story.id}>
+
+//           </div>
+//        </button>
+//     </div>
+//   )
+// }
+
+// export default StoryCard;
+//need props, which is why we imported storycard into storiespage
+//this card is for the browse all stories page
+import React from "react";
+import "./components.css";
+import { useNavigate } from "react-router-dom";
+
+function StoryCard(props) {
+  // const { title, event } = props;
+  const story = props.story;
+  const navigate = useNavigate();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    navigate(`/stories/${story._id}`); //redirects to the individual story page
+  };
+  //does essentially the samething as the StoriesPage where we check if the input/title exists
+
+  return (
+    // <div className="story-card" style={{maxWidth:"20%"}}>
+    //     <button className="card-button" onClick={handleClick}>
+    //     <h1 className="card-title"> Title: {story.title} </h1>
+    //     <div key={story.id}>
+
+    //       </div>
+    //    </button>
+    // </div>
+
+    <div className="story-card">
+      <div className="card">
+        <image
+          className="card-img-top"
+          src="https://i.imgur.com/t1lXeFj.png"
+          alt="Card image cap"
+          // Set a fixed height for the card image
+        />
+
+        <div className="card-body">
+          <h5 className="card-title btn btn-primary" onClick={handleClick}>
+            {story.title}
+          </h5>
+
+          <p className="card-text">{story.event}</p>
+          {/* <h1 className="btn btn-primary" onClick={handleClick}>
+          Go somewhere
+        </h1>  */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default StoryCard;
